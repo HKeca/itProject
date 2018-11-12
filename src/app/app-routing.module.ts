0c5400652e092b938fac1bd2796625e29d16e368
Laura Saravia <lauravs0701@gmail.com>
2018-11-12 17:59:35 -0500

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nab-bar.component';
+import { PortfolioComponent } from './Portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: '', component: appComponent },
+{ path: 'portfolio', component: PortfolioComponent },
+{ path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
